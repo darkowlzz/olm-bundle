c3ca814c11aaad5355ba8db736886c3abeb3694d
Sunny <darkowlzz@protonmail.com>
2020-10-15 22:01:08 +0530

    Install yq & add update tests to check annotations
    Add test to check the values of the annotations in the generated
    bundles.

--- test.sh
+++ test.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+YQ="./bin/yq"
+
 # Helper for printing error.
-echoerr() { printf "ERROR: %s\n" "$*" >&2; }
+echoerr() {
+	printf "ERROR: %s\n" "$*" >&2;
+	exit 1
+}
 
 # $1 - Absolute path of an expected directory. 
 check_dir_exists () {
 	if [ ! -d $1 ]; then
 		echoerr "expected $1 to be created"
-		exit 1
 	fi
 }
 
@@ -21,15 +25,41 @@ check_manifests_copied () {
 		filename="$2/$(basename $filepath)"
 		if [ ! -f "$filename" ]; then
 			echoerr "expected $filename to be created"
-			exit 1
 		fi
 	done
+}
+
+# $1 - Absolute path of the output bundle dir.
+check_annotations () {
+	annotationsFile="$1/metadata/annotations.yaml"
+
+	# Read the values from the annotations file. Check the attribues that
+	# are passed to OPM, if they have the expected value.
+	GOT_CHANNELS=$($YQ r $annotationsFile annotations[operators.operatorframework.io.bundle.channels.v1])
+	GOT_DEFAULT_CHANNEL=$($YQ r $annotationsFile annotations[operators.operatorframework.io.bundle.channel.default.v1])
+	GOT_PACKAGE=$($YQ r $annotationsFile annotations[operators.operatorframework.io.bundle.package.v1])
+	GOT_MANIFESTS=$($YQ r $annotationsFile annotations[operators.operatorframework.io.bundle.manifests.v1])
+	# Got and want manifests. Get the basename only.
+	GM=$(basename $GOT_MANIFESTS)
+	WM=$(basename $MANIFESTS_DIR)
+
+	if [ "$GOT_CHANNELS" != "$CHANNELS" ]; then
+		echoerr "expected channels to be $CHANNELS, got $GOT_CHANNELS"
+	fi
+	if [ "$GOT_DEFAULT_CHANNEL" != "$DEFAULT_CHANNEL" ]; then
+		echoerr "expected default channel to be $DEFAULT_CHANNEL, got $GOT_DEFAULT_CHANNEL"
+	fi
+	if [ "$GOT_PACKAGE" != "$PACKAGE" ]; then
+		echoerr "expected package to be $PACKAGE, got $GOT_PACKAGE"
+	fi
+	if [ "$GM" != "$WM" ]; then
+		echoerr "expected manifests to be $WM, got $GM"
+	fi
 }
 
 check_file_exists () {
 	if [ ! -f $1 ]; then
 		echoerr "expected $$1 to be created"
-		exit 1
 	fi
 }
 
@@ -42,6 +72,7 @@ test_new_output_dir () {
 
 	export MANIFESTS_DIR=$PWD/testdata/bundle/manifests
 	export CHANNELS=stable
+	export DEFAULT_CHANNEL=stable
 	export PACKAGE=test-operator
 	export OUTPUT_DIR=$TEST_OUTPUT_DIR/0.0.2
 
@@ -59,6 +90,9 @@ test_new_output_dir () {
 	WANT_DOCKERFILE="$(dirname $OUTPUT_DIR)/bundle-$(basename $OUTPUT_DIR).Dockerfile"
 	check_file_exists $WANT_DOCKERFILE
 
+	# Check if the annotations are as expected.
+	check_annotations $OUTPUT_DIR
+
 	# Cleanup.
 	rm -rf $TEST_OUTPUT_DIR
 }
@@ -71,6 +105,7 @@ test_add_new_bundle () {
 
 	export MANIFESTS_DIR=$PWD/testdata/bundle/manifests
 	export CHANNELS=stable
+	export DEFAULT_CHANNEL=stable
 	export PACKAGE=memcached
 	export OUTPUT_DIR=$TEST_OUTPUT_DIR/0.0.2
 
@@ -88,14 +123,18 @@ test_add_new_bundle () {
 	WANT_DOCKERFILE="$(dirname $OUTPUT_DIR)/bundle-$(basename $OUTPUT_DIR).Dockerfile"
 	check_file_exists $WANT_DOCKERFILE
 
+	# Check if the annotations are as expected.
+	check_annotations $OUTPUT_DIR
+
 	# Cleanup.
 	rm -rf $OUTPUT_DIR
 	rm $WANT_DOCKERFILE
 }
 
-# Install opm.
+# Install opm and yq.
 make opm
 sudo cp bin/opm /usr/local/bin
+sudo cp bin/yq /usr/local/bin
 
 # Run tests.
 test_new_output_dir
